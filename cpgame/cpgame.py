b386edd5fec2b3f770b3780903b17a7b334b84ae
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-08-23 06:48:38 +0100

    Update cpgame.py
    changed commands to include `cp`

--- cpgame/cpgame.py
+++ cpgame/cpgame.py
@@ -20,7 +20,7 @@ class CPGame:
         #    timeout=None
         #)
     
-    @commands.command(name="number")
+    @commands.command(name="cpnumber")
     async def check_number(self, ctx):
         channel_config = self.config.channel(channel=ctx.channel)
         if await channel_config.active():
@@ -30,7 +30,7 @@ class CPGame:
             await ctx.send(f"There is no live game on.")
     
     @checks.mod_or_permissions(manage_channels=True)
-    @commands.command(name="start", case_insensitive=True)
+    @commands.command(name="cpstart", case_insensitive=True)
     async def start_game(self, ctx, start: int=10):
         """Start the game in this channel from that number. It's highly recommended to use a dedicated channel for this as any other message will be deleted."""
         if not 10 <= start < 3500:
@@ -44,7 +44,7 @@ class CPGame:
         await channel_config.last_trainer_id.set(None)
     
     @checks.mod_or_permissions(manage_channels=True)
-    @commands.command(name="pause", case_insensitive=True)
+    @commands.command(name="cppause", case_insensitive=True)
     async def pause_game(self, ctx):
         """Pause the CP Game"""
     
@@ -62,7 +62,7 @@ class CPGame:
             await ctx.send("No active game! Nothing to do.")
     
     @checks.mod_or_permissions(manage_channels=True)
-    @commands.command(name="continue", case_insensitive=True)
+    @commands.command(name="cpcontinue", case_insensitive=True)
     async def continue_game(self, ctx):
         """Continute an already started game"""
     
@@ -75,7 +75,7 @@ class CPGame:
             await ctx.send("No valid game! Please use the `start` command to create a new game.")
     
     @checks.mod_or_permissions(manage_channels=True)
-    @commands.command(name="end", case_insensitive=True)
+    @commands.command(name="cpend", case_insensitive=True)
     async def end_game(self, ctx):
         """End the existing game"""
         channel_config = self.config.channel(channel=ctx.channel)
